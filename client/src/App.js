c349fb8a4e177511652214c5c22a6dbfb638c0b6
Sarah Lash <sarah.lash92@gmail.com>
2023-03-02 22:06:15 +0100

    First commit
    -Basic 3d scene
    -- main character moving on keys
    
    -chat component toggle working
    -- able to write text
    -- still not connected to the API

---
+++ client/src/App.js
@@ -0,0 +1,38 @@
+import React , { useState } from 'react';
+import './App.css';
+import Scene from './components/scene/scene';
+import ChatWindow from './components/chatWindow/chatWindow';
+
+
+
+
+function App() {
+
+  const [showChat, setShowChat] = useState(false);
+
+  const openChatWindow = () => {
+    setShowChat(true);
+  };
+
+  const closeChatWindow = () => {
+    setShowChat(false);
+  };
+
+  const [messages, setMessages] = useState([]);
+
+  const addMessage = (text, sender) => {
+    setMessages([...messages, { text, sender }]);
+  };
+
+
+  return (
+<div className="container">
+      <Scene openChatWindow={openChatWindow} />
+      {showChat && (
+        <ChatWindow messages={messages} addMessage={addMessage} closeChatWindow={closeChatWindow} />
+      )}
+    </div>
+  );
+}
+
+export default App;
