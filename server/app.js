b7e3735f6df568850de637714f04659130019305
Sarah Lash <sarah.lash92@gmail.com>
2023-03-03 10:58:39 +0100

    server connected to inworld

--- server/app.js
+++ server/app.js
@@ -1,14 +1,53 @@
 const express = require('express');
+const { InworldClient } = require('@inworld/nodejs-sdk');
+
 const app = express();
 
+const client = new InworldClient();
+client.setApiKey({
+  key: 't3hIk1GR6ruRVHqccmCUxzhduCT1ZOOf',
+  secret:'1ZUDPp2cWxg31FmVB1AnWSqTsWecbhX13sqCyhzTocHw9nF2sMRMRmCkbjWoz5S5',
+});
+client.setUser({
+  fullName: 'Slash',
+});
+client.setConfiguration({
+  capabilities: { audio: true, emotions: true },
+});
+client.setScene('workspaces/default-w5pin-cntlbebptkhe0fwa/scenes/the_caterpillars_mushroom');
 
-const PORT = 3001;
+client.setOnError((err) => {
+  console.error(err);
+});
 
-app.get("/" , (req, res) => {
-  
-  res.send("Working");
-})
+client.setOnMessage((packet) => {
+  if(packet.text && packet.text.text){
+    console.log("this is the msg text", packet.text.text);
+  }
 
-app.listen(PORT, () => {
-  console.log(`Server Listening on port ${PORT}`)
-})
+});
+
+const connection = client.build();
+
+// Send a message to the InworldClient and log the response message to the console
+async function sendMessage(message) {
+  await connection.sendText(message);
+}
+
+const character = connection.getCurrentCharacter();
+
+async function charactersName(){
+  await character.getDisplayName();
+}
+
+// Express route to send a message to the InworldClient and log the response message to the console
+app.get('/', async (req, res) => {
+  const message = 'Hey, how are you?';
+  await sendMessage(message);
+  res.send('Message sent successfully.');
+});
+
+// Start the Express server
+app.listen(3001, () => {
+  console.log('Server started on port 3001');
+});
